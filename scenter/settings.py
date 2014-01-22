91e998633aecfe486370f845b0237a5ef79fb6f7
DenXX <denissavenkov@gmail.com>
2014-01-22 01:37:37 -0500

    Migrating to PostGIS database

--- scenter/settings.py
+++ scenter/settings.py
@@ -13,16 +13,13 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.contrib.gis.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
         'NAME': 'scenter',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': 'scenter',
         'PASSWORD': 'scenterpass',
         'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
-        'OPTIONS': {
-            'init_command': 'SET storage_engine=MyISAM',
-        }
     }
 }
 
